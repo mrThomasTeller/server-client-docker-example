cca791825a2570fafc6e96cc71d51dd41366df62
abakharev <abakharev@sharevault.com>
2023-11-03 00:05:43 +0300

    update commands

--- commands.sh
+++ commands.sh
@@ -6,3 +6,13 @@ docker compose up --build
 
 # собрать контейнеры из образов и запустить в фоне
 docker compose up --build -d
+
+# деплой на чистый сервер:
+git clone ...
+cd ...
+curl -fsSL https://get.docker.com | sh
+docker compose up --build -d
+
+# обновление задопленного приложения
+git pull
+docker compose up --build -d
